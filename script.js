118f83a30ffb31cfa4054ec773af4bfa47772947
Shubham Chhetri <shubhamchh3@gmail.com>
2021-05-04 12:22:25 +0000

    Add existing file

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
-function compute()
+function compute() {
+  var principal = document.getElementById("principal").value;
+  if (principal < 1) {
+    alert("Enter a positive number");
+    document.getElementById("principal").select();
+    document.getElementById("principal").focus();
+    return;
+  }
+  var rate = document.getElementById("rate").value;
+  var years = document.getElementById("years").value;
+  var interest = principal * years * rate / 100
+  var currentYear = new Date().getFullYear();
+  var totalyears = +currentYear + +years;
+
+
+  Line1 = '<br>If you deposit<mark> ' + principal + '</mark><br>';
+  Line2 = ' at an interest rate of <mark>' + rate + '%' + '</mark><br>';
+  Line3 = ' you will receive an amount of <mark>' + interest + '</mark><br>';
+  Line4 = ' in the year<mark> ' + totalyears + '</mark><br>';
+
+  document.getElementById("result").innerHTML = Line1 + Line2 + Line3 + Line4;
+}
+
+function updateRate() 
 {
-    p = document.getElementById("principal").value;
-    
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
 }
         
